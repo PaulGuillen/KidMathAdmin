78328bb41115667337cb09ce1caf61cb23e005af
paulguillen190 <paulguillen190@gmail.com>
2022-05-01 14:57:58 -0500

    SomesChanges

--- app/src/main/java/devpaul/business/kidmathadmin/fragments/StudentsFragment.kt
+++ app/src/main/java/devpaul/business/kidmathadmin/fragments/StudentsFragment.kt
@@ -1,18 +1,24 @@
 package devpaul.business.kidmathadmin.fragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import devpaul.business.kidmathadmin.Constants
+import devpaul.business.kidmathadmin.MainActivity
 import devpaul.business.kidmathadmin.R
 import devpaul.business.kidmathadmin.adapter.OperationsAdapter
 import devpaul.business.kidmathadmin.adapter.UserAdapter
@@ -23,6 +29,7 @@ class StudentsFragment : Fragment() {
 
     var TAG = "ViewAllSection"
 
+    var btnSalir : Button? = null
     var myView: View? = null
     private val db = Firebase.firestore
     private var recyclerViewAll: RecyclerView? = null
@@ -30,12 +37,14 @@ class StudentsFragment : Fragment() {
     //ViewAllSection
     var viewAllList = ArrayList<User>()
     private var viewAllAdapter: UserAdapter? = null
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        auth = Firebase.auth
         // Inflate the layout for this fragment
         myView = inflater.inflate(R.layout.fragment_students, container, false)
 
@@ -44,6 +53,15 @@ class StudentsFragment : Fragment() {
             LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
         viewAllAdapter = UserAdapter(requireContext(), viewAllList)
         recyclerViewAll?.adapter = viewAllAdapter
+
+        btnSalir = myView?.findViewById(R.id.btn_salir)
+        btnSalir?.setOnClickListener {
+            auth.signOut()
+            val i = Intent(requireContext(), MainActivity::class.java)
+            Toast.makeText(requireContext(),"Vuelva pronto", Toast.LENGTH_LONG).show()
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK // Eliminar el historial de pantallas
+            startActivity(i)
+        }
 
         return myView
     }
